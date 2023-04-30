2551a60332f8ff6126e9e6f214a3e2aedc356bf1
kshwi <kshwiss@gmail.com>
2023-04-30 15:46:07 -0500

    completed the inquirer part, now working on getting the sql to work with node

--- Main/db/seeds.sql
+++ Main/db/seeds.sql
@@ -1,14 +1,14 @@
-INSERT INTO department (id, name)
-VALUES (1, "Finance"),
-       (2, "Coding"),
-       (3, "Fishing");
+INSERT INTO department ( name)
+VALUES ("Finance"),
+       ("Coding"),
+       ("Fishing");
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES (32, "Chief of fishing", 5000, 3),
-       (6, "Code tester", 4000, 2),
+VALUES (377, "Chief of fishing", 5000, 3),
+       (60000, "Code tester", 4000, 2),
        (666, "Money counter", 6000, 1);
 
-INSERT INTO employee (id, first_name. last_name, role_id, manager_id)
-VALUES (7, "Johnny", "Shandito", 76),
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (7, "Johnny", "Shandito", 76, 89),
        (678, "Lazy", "Guy", 21, 12),
-       (22, "Jim","Umbridge", 42);
+       (22, "Jim","Umbridge", 42, 43);
